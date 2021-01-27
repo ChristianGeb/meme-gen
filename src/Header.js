c11f865e06d3b8ac109dd86dd26ee870f8de4d80
ChrisFennek <christian.gebhardt@online.de>
2021-01-27 18:05:02 +0100

    Header and MemeGen

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img
+        src="http://placekitten.com/200/300"
+        alt="lol"
+      />
+      <p>Meme Generator</p>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/MemeGen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MemeGen extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h1>Big meme</h1>
+    )
+  }
+}
+
+export default MemeGen
